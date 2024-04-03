425ba6623748839d8b6270db39146a8d97634a97
BHImran <imran689690@gmail.com>
2024-04-04 02:31:18 +0600

    my frist commit

---
+++ main.py
@@ -0,0 +1,13 @@
+num1=int(input("enter the frist number:"));
+num2=int(input("enter the second number:"));
+ope=input("Enter your Operator :");
+if ope == "+":
+    print(num1+num2)
+elif ope == "-":
+    print(num1-num2);
+elif ope =="*":
+    print(num1*num2);
+elif ope =="/" :
+    print(num1/num2);
+else:
+    print("indvaild operator")
